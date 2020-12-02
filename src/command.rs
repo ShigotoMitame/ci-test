359b265572e5a763d72a3a506a422ac3de8759bc
Levi Wright <gitcommits@mita.me>
2020-12-02 00:13:57 +0000

    Add builder and do other things

---
+++ src/command.rs
@@ -0,0 +1,321 @@
+use std::iter::Map;
+use std::ops::BitOr;
+
+#[derive(Debug)]
+pub enum ClockDirection {
+    Rising,
+    Falling,
+}
+
+#[derive(Debug)]
+pub enum BitDirection {
+    LsbFirst,
+    MsbFirst,
+}
+
+#[derive(Debug)]
+struct FullDataShiftOptions {
+    read_clock_direction: ClockDirection,
+    write_clock_direction: ClockDirection,
+    bit_direction: BitDirection,
+    write_tdi: bool,
+    read_tdo: bool,
+    write_tms: bool,
+}
+
+impl Default for FullDataShiftOptions {
+    fn default() -> Self {
+        FullDataShiftOptions {
+            read_clock_direction: ClockDirection::Rising,
+            write_clock_direction: ClockDirection::Rising,
+            bit_direction: BitDirection::MsbFirst,
+            write_tdi: false,
+            read_tdo: false,
+            write_tms: false,
+        }
+    }
+}
+
+impl Into<u8> for FullDataShiftOptions {
+    fn into(self) -> u8 {
+        let mut byte = 0;
+        byte |= match self.write_clock_direction {
+            ClockDirection::Rising => 0x00,
+            ClockDirection::Falling => 0x01,
+        };
+        byte |= match self.read_clock_direction {
+            ClockDirection::Rising => 0x00,
+            ClockDirection::Falling => 0x04,
+        };
+        byte |= match self.bit_direction {
+            BitDirection::MsbFirst => 0x00,
+            BitDirection::LsbFirst => 0x08,
+        };
+        byte |= match self.write_tdi {
+            false => 0x00,
+            true => 0x10,
+        };
+        byte |= match self.read_tdo {
+            false => 0x00,
+            true => 0x20,
+        };
+        byte |= match self.write_tms {
+            false => 0x00,
+            true => 0x40,
+        };
+
+        byte
+    }
+}
+
+#[derive(Debug)]
+pub struct DataShiftOptions {
+    pub clock_direction: ClockDirection,
+    pub bit_direction: BitDirection
+}
+
+impl Into<FullDataShiftOptions> for DataShiftOptions{
+    fn into(self) -> FullDataShiftOptions {
+        FullDataShiftOptions {
+            read_clock_direction: self.clock_direction,
+            bit_direction: self.bit_direction,
+            read_tdo: true,
+            ..Default::default()
+        }
+    }
+}
+
+impl Into<u8> for DataShiftOptions {
+    fn into(self) -> u8 {
+        Into::<FullDataShiftOptions>::into(self).into()
+    }
+}
+
+#[derive(Debug)]
+pub enum PinRange {
+    High,
+    Low,
+}
+
+#[derive(Debug)]
+pub enum PinValue {
+    High,
+    Low,
+}
+
+#[repr(transparent)]
+#[derive(Debug)]
+pub struct PinValueArray([PinValue; 8]);
+
+impl Into<u8> for PinValueArray {
+    fn into(self) -> u8 {
+        self.0
+            .iter()
+            .enumerate()
+            .map(|(i, val)| match val {
+                PinValue::High => 0 << i,
+                PinValue::Low => 1 << i,
+            })
+            .fold(0u8, |acc, val| acc | val)
+    }
+}
+
+#[derive(Debug)]
+pub enum PinDirection {
+    Input,
+    Output,
+}
+
+#[repr(transparent)]
+#[derive(Debug)]
+pub struct PinDirectionArray([PinDirection; 8]);
+
+impl Into<u8> for PinDirectionArray {
+    fn into(self) -> u8 {
+        self.0
+            .iter()
+            .enumerate()
+            .map(|(i, val)| match val {
+                PinDirection::Input => 0 << i,
+                PinDirection::Output => 1 << i,
+            })
+            .fold(0u8, |acc, val| acc | val)
+    }
+}
+
+#[derive(Debug)]
+pub enum Command {
+    ReadDataShiftBits {
+        options: DataShiftOptions,
+        length: u8,
+    },
+    ReadDataShiftBytes {
+        options: DataShiftOptions,
+        length: u16,
+    },
+    WriteDataShiftBits {
+        options: DataShiftOptions,
+        bits: u8,
+        length: u8,
+    },
+    WriteDataShiftBytes {
+        options: DataShiftOptions,
+        bytes: Vec<u8>,
+    },
+    SetBits {
+        range: PinRange,
+        value: PinValueArray,
+        direction: PinDirectionArray,
+    },
+    ReadBits {
+        range: PinRange,
+    },
+    SetLoopback {
+        enable: bool,
+    },
+    SetClockDivisor {
+        divisor: u16,
+    },
+}
+
+impl Command {
+    pub fn expected_response_length(&self) -> usize {
+        match self {
+            Self::ReadDataShiftBits {
+                options,
+                length: _,
+            } => 1,
+            Self::WriteDataShiftBits {
+                options,
+                bits: _,
+                length: _,
+            } => 0,
+            Self::WriteDataShiftBytes { options: _, bytes: _ } => 0,
+            Self::ReadDataShiftBytes { options: _, length } => length.to_owned() as usize,
+            Self::SetBits {
+                range: _,
+                value: _,
+                direction: _,
+            } => 0,
+            Self::ReadBits { range: _ } => 1,
+            Self::SetLoopback { enable: _ } => 0,
+            Self::SetClockDivisor { divisor: _ } => 0,
+        }
+    }
+}
+
+impl Into<Vec<u8>> for Command {
+    fn into(self) -> Vec<u8> {
+        match self {
+            Self::WriteDataShiftBits {
+                options,
+                bits,
+                length,
+            } => {
+                let opcode = Into::<u8>::into(options) | 0x02;
+
+                vec![opcode, length - 1, bits]
+            }
+            Self::ReadDataShiftBits {
+                options,
+                length,
+            } => {
+                let opcode = Into::<u8>::into(options) | 0x02;
+
+                vec![opcode, length - 1]
+            }
+            Self::WriteDataShiftBytes { options, bytes } => {
+                let opcode = Into::<u8>::into(options);
+
+                let mut result = vec![opcode];
+                result.extend_from_slice(&((bytes.len() - 1) as u16).to_le_bytes());
+                result.extend(bytes);
+
+                result
+            }
+            Self::ReadDataShiftBytes { options, length } => {
+                let opcode = Into::<u8>::into(options);
+
+                let mut result = vec![opcode];
+                result.extend_from_slice(&(length - 1).to_le_bytes());
+
+                result
+            }
+            Self::SetBits {
+                range,
+                value,
+                direction,
+            } => {
+                let opcode = match range {
+                    PinRange::Low => 0x80,
+                    PinRange::High => 0x82,
+                };
+
+                vec![opcode, value.into(), direction.into()]
+            }
+            Self::ReadBits { range } => {
+                let opcode = match range {
+                    PinRange::Low => 0x81,
+                    PinRange::High => 0x83,
+                };
+
+                vec![opcode]
+            }
+            Self::SetLoopback { enable } => {
+                let opcode = match enable {
+                    true => 0x84,
+                    false => 0x85,
+                };
+
+                vec![opcode]
+            }
+            Command::SetClockDivisor { divisor } => {
+                let mut result = vec![0x86];
+                result.extend_from_slice(&divisor.to_le_bytes());
+                result
+            }
+        }
+    }
+}
+
+impl IntoIterator for Command {
+    type Item = u8;
+
+    type IntoIter = std::vec::IntoIter<u8>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let bytes: Vec<u8> = self.into();
+
+        bytes.into_iter()
+    }
+}
+
+#[derive(Debug)]
+pub struct CommandList(pub Vec<Command>);
+
+impl IntoIterator for CommandList {
+    type Item = u8;
+
+    type IntoIter = std::vec::IntoIter<u8>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let mut result = Vec::new();
+        for command in self.0 {
+            result.extend(command)
+        }
+
+        result.into_iter()
+    }
+}
+
+impl Into<Vec<u8>> for CommandList {
+    fn into(self) -> Vec<u8> {
+        self.into_iter().collect()
+    }
+}
+
+impl CommandList {
+    pub fn expected_response_length(self) -> usize {
+        self.0.iter().map(|cmd| cmd.expected_response_length()).fold(0, |acc, cur| acc + cur)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,152 @@
+mod command;
+
+use self::command::{Command, ClockDirection, BitDirection, DataShiftOptions, CommandList};
+
+#[derive(Debug)]
+pub struct Builder {
+    commands: Vec<Command>
+}
+
+impl Builder {
+    pub fn new() -> Self {
+        Builder {
+            commands: vec!()
+        }
+    }
+
+    pub fn write_data(self, data: Vec<u8>) -> WriteBuilder {
+        WriteBuilder {
+            parent: self,
+            data,
+            clock_direction: ClockDirection::Rising,
+            bit_direction: BitDirection::MsbFirst,
+        }
+    }
+
+    pub fn read_data(self, length: u16) -> ReadBuilder {
+        ReadBuilder {
+            parent: self,
+            length,
+            clock_direction: ClockDirection::Rising,
+            bit_direction: BitDirection::MsbFirst,
+        }
+    }
+        
+    pub fn build(self) -> CommandList {
+        CommandList(self.commands)
+    }
+}
+
+#[derive(Debug)]
+pub struct ReadBuilder {
+    parent: Builder,
+    length: u16,
+    clock_direction: ClockDirection,
+    bit_direction: BitDirection,
+}
+
+impl ReadBuilder {
+    pub fn with_clock_direction(self, direction: ClockDirection) -> Self {
+        ReadBuilder {
+            clock_direction: direction,
+            ..self
+        }
+    }
+
+    pub fn with_bit_direction(self, direction: BitDirection) -> Self {
+        ReadBuilder {
+            bit_direction: direction,
+            ..self
+        }
+    }
+
+    fn commit(mut self) -> Builder {
+        self.parent.commands.push(
+            Command::ReadDataShiftBytes {
+                options: DataShiftOptions {
+                    clock_direction: self.clock_direction,
+                    bit_direction: self.bit_direction,
+                },
+                length: self.length,
+            }
+        );
+
+        self.parent
+    }
+
+    pub fn then(self) -> Builder { 
+        self.commit()
+    }
+
+    pub fn build(self) -> CommandList {
+        self.commit().build()
+    }
+}
+
+#[derive(Debug)]
+pub struct WriteBuilder {
+    parent: Builder,
+    data: Vec<u8>,
+    clock_direction: ClockDirection,
+    bit_direction: BitDirection,
+}
+
+impl WriteBuilder {
+    pub fn with_clock_direction(self, direction: ClockDirection) -> Self {
+        WriteBuilder {
+            clock_direction: direction,
+            ..self
+        }
+    }
+
+    pub fn with_bit_direction(self, direction: BitDirection) -> Self {
+        WriteBuilder {
+            bit_direction: direction,
+            ..self
+        }
+    }
+
+    fn commit(mut self) -> Builder {
+        self.parent.commands.push(
+            Command::WriteDataShiftBytes {
+                options: DataShiftOptions {
+                    clock_direction: self.clock_direction,
+                    bit_direction: self.bit_direction,
+                },
+                bytes: self.data,
+            }
+        );
+
+        self.parent
+    }
+
+    pub fn then(self) -> Builder { 
+        self.commit()
+    }
+
+    pub fn build(self) -> CommandList {
+        self.commit().build()
+    }
+}
+
+
+
 #[cfg(test)]
-mod tests {
+mod write_builder_tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn syntax_test() {
+        let data = vec![0x10, 0x01, 0x20, 0x01];
+        
+        let commands = Builder::new()
+            .write_data(data)
+            .with_clock_direction(ClockDirection::Rising) 
+            .with_bit_direction(BitDirection::MsbFirst)
+            .build();
+         
+        let command_bytes: Vec<u8> = commands.into_iter().collect();
+        
+        assert_eq!(command_bytes, vec![0x10, 0x03, 0x00, 0x10, 0x01, 0x20, 0x01]);
     }
 }
